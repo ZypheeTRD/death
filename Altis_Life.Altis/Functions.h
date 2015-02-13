5b9fd059f2d8a645c271e8f82a8988fb1c4fb919
killerclownlolpoll <ahhhazombie@gmail.com>
2015-02-13 00:20:03 -0800

    Virtual Item Icons

--- Altis_Life.Altis/Functions.h
+++ Altis_Life.Altis/Functions.h
@@ -147,6 +147,7 @@ class Life_Client_Core
 		class weaponShopCfg {};
 		class vehicleWeightCfg {};
 		class houseConfig {};
+        class itemIcon {};
 		
 		//Clothing Store Configs
 		class clothing_cop {};
